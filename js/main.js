a2f265400504ac6781b006b9ca853a6fc8cc8db2
Christian0218 <64560009+Christian0218@users.noreply.github.com>
2020-05-13 14:24:19 -0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+const typed = new Typed('.typed', {
+	strings: [
+		'<i class="algo">Comunidad</i>',
+		'<i class="algo">Mundo</i>'
+	],
+
+	typeSpeed: 100, 
+	startDelay: 300, 
+	backSpeed: 75, 
+	smartBackspace: true, 
+	shuffle: false, 
+	backDelay: 1500, 
+	loop: true,
+	loopCount: false, 
+	showCursor: true, 
+	cursorChar: '.', 
+	contentType: 'html', 
+});
